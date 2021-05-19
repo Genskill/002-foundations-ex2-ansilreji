2ad3d68d18eb9704e39466556626c4622f119e54
ansilreji <84070903+ansilreji@users.noreply.github.com>
2021-05-19 19:47:37 +0530

    patterns.c

---
+++ patterns.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+₹include<cs50.h>
+int main(void) 
+{
+int pattern=get int("Enter the pattern number ") ;
+int row=get int(" Enter number of rows");
+if(pattern=1) 
+{
+for(int i=1; i<=row; i++) 
+{ for(int s=0; s<=i; s++) 
+printf("#");
+printf("\n");
+}
+else if(pattern=2) 
+{
+for(int i=1; i<=n; i++)
+{
+for(int j=1; j <= n-i; j++)
+printf(" "); 
+for(int k=1; k <= i; k++)
+printf("#");
+printf("\n") ;
+}
+}
