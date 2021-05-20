ca8050cee55c2e7e5724e27f0caaf3c02db5b72e
ansilreji <84070903+ansilreji@users.noreply.github.com>
2021-05-20 09:28:39 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "cs50.h"
 int main(void) 
-{ int n;
+{ 
 int pattern = get int("Enter the pattern number ");
 int row = get int(" Enter number of rows");
 if(pattern=1) 
@@ -14,10 +14,10 @@ printf("\n");
 }
 else if(pattern=2) 
 {
-for(int i=1; i<=n; i++)
+for(int i=1; i<=row; i++)
 {
-for(int j=1; j <= n-i; j++)
-printf(" "); 
+for(int j=1; j <= row -i; j--)
+printf(" "); }{
 for(int k=1; k <= i; k++)
 printf("#");
 printf("\n") ;
